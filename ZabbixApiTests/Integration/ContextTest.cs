18b7fbb87bd53a9135a00e1b1fbeed1fede75a8d
CG2000\bbemfica <bbemfica@compugraf.com.br>
2018-01-02 14:07:11 -0200

    regression to .net 4.5

---
+++ ZabbixApiTests/Integration/ContextTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using ZabbixApi;
+
+namespace ZabbixApiTests.Integration
+{
+    public class ContextTest : BaseIntegrationTest
+    {
+        private Exception ex;
+
+        [Fact]
+        public void MustNotLoginWithWrongUserData()
+        {
+            var url = "1";
+            var username = "1";
+            var password = "1";
+
+            try
+            {
+                var ctx = new Context(url, username, password);
+            }
+            catch (Exception _ex)
+            {
+                ex = _ex;
+            }
+
+            Assert.NotNull(ex);
+            
+        }
+
+        [Fact]
+        public void MustLoginWithRightUserData()
+        {
+            var ctx = new Context(this.url, this.user, this.password);
+            Assert.NotNull(ctx);
+        }
+    }
+}
