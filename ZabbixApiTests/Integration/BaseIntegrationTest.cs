21c79425a646172c29b2b6ef6c2f94be0f3bb1e2
Henrique Clemente <hclemente@compugraf.com.br>
2018-10-30 12:01:46 -0300

    wait for ccontainer up before run tests

--- ZabbixApiTests/Integration/BaseIntegrationTest.cs
+++ ZabbixApiTests/Integration/BaseIntegrationTest.cs
@@ -4,7 +4,7 @@ namespace ZabbixApiTests.Integration
 {
     public class BaseIntegrationTest
     {
-        protected string url = "http://127.0.0.1:43210/api_jsonrpc.php";
+        protected string url = "http://MyZabbixServer:43210/api_jsonrpc.php";
         protected string user = "Admin";
         protected string password = "zabbix";
         protected IContext context;
